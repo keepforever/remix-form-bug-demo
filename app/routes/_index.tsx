7df31141ccc958866c8840feb085e1237cd45aa1
Brian Cilenti <briancilenti@gmail.com>
2024-07-03 18:07:13 -0400

    fix: demo bug

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,13 @@
-import type { MetaFunction } from "@remix-run/node";
+import type { ActionFunctionArgs, LoaderFunctionArgs } from "@remix-run/node";
+import { json, MetaFunction } from "@remix-run/node";
+import {
+  Form,
+  useLoaderData,
+  useActionData,
+  useNavigation,
+  Link,
+} from "@remix-run/react";
+import { useRef, useEffect } from "react";
 
 export const meta: MetaFunction = () => {
   return [
@@ -7,42 +16,71 @@ export const meta: MetaFunction = () => {
   ];
 };
 
+// eslint-disable-next-line no-empty-pattern, @typescript-eslint/no-unused-vars
+export const loader = async (args: LoaderFunctionArgs) => {
+  const resp = [
+    { id: 1, name: "USA" },
+    { id: 2, name: "Canada" },
+  ];
+  return json({ countries: resp });
+};
+
+export const action = async ({ request }: ActionFunctionArgs) => {
+  const formData = await request.formData();
+  const name = formData.get("name") as string;
+  const code = formData.get("code") as string;
+  console.log("\n", `name, code = `, name, code, "\n");
+
+  return json({ message: "Country created", resp: { name, code } });
+};
+
 export default function Index() {
+  const loaderData = useLoaderData<typeof loader>();
+  const actionData = useActionData<typeof action>();
+  const formRef = useRef<HTMLFormElement>(null);
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === "submitting";
+
+  useEffect(() => {
+    if (actionData?.message === "Country created") {
+      console.log("\n", `formRef.current = `, formRef.current, "\n");
+      formRef.current?.reset();
+    }
+  }, [actionData]);
+
   return (
-    <div className="font-sans p-4">
-      <h1 className="text-3xl">Welcome to Remix</h1>
-      <ul className="list-disc mt-4 pl-6 space-y-2">
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/start/quickstart"
-            rel="noreferrer"
-          >
-            5m Quick Start
-          </a>
-        </li>
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/start/tutorial"
-            rel="noreferrer"
-          >
-            30m Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/docs"
-            rel="noreferrer"
-          >
-            Remix Docs
-          </a>
-        </li>
+    <div className="flex flex-col gap-4 p-4">
+      <h1 className="text-3xl">Hello Portal Spike</h1>
+
+      <ul>
+        {loaderData.countries.map((country) => (
+          <li key={country.id}>
+            <Link
+              to={`/countries/${country.id}`}
+              className="text-blue-500 hover:underline"
+            >
+              {country.name}
+            </Link>
+          </li>
+        ))}
       </ul>
+
+      <Form method="post" ref={formRef} className="flex flex-col max-w-3xl">
+        <h2 className="text-xl font-semibold mb-4">Add a new country</h2>
+        <label className="block mb-2">
+          <span className="text-gray-700">Name</span>
+          <input type="text" name="name" />
+        </label>
+        <label className="block mb-4">
+          <span className="text-gray-700">Code</span>
+          <input type="text" name="code" />
+        </label>
+        <div className="flex items-center gap-2 flex-wrap">
+          <button type="submit" disabled={isSubmitting}>
+            {isSubmitting ? "Submitting..." : "Submit"}
+          </button>
+        </div>
+      </Form>
     </div>
   );
 }
